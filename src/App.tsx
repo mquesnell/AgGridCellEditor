e93fd7b31c9c1d1ffa8f2ece88a201c316833fb2
Mike Quesnell <michael.quesnell@us.af.mil>
2021-12-14 16:43:03 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import "./styles.css";
+
+import { AgGridColumn, AgGridReact } from "ag-grid-react";
+
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+
+import { BasicCellEditor } from "./BasicCellEditor";
+export default function App() {
+  const rowData = [
+    { make: "Toyota", model: "Celica", price: 3 },
+    { make: "Ford", model: "Mondeo", price: 3200 },
+    { make: "Porsche", model: "Boxter", price: 7200 }
+  ];
+
+  return (
+    <div className="App">
+      <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
+        <AgGridReact
+          rowData={rowData}
+          frameworkComponents={{
+            basicCellEditor: BasicCellEditor
+          }}
+        >
+          <AgGridColumn field="make"></AgGridColumn>
+          <AgGridColumn field="model"></AgGridColumn>
+          <AgGridColumn
+            field="price"
+            sortable={true}
+            cellEditor="basicCellEditor"
+            editable={true}
+          ></AgGridColumn>
+        </AgGridReact>
+      </div>
+    </div>
+  );
+}
